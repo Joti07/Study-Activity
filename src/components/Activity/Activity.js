6d3862e675a329083b14fb4dea2ab35197f55488
Joti <tanjilajoti@gmail.com>
2022-09-29 03:14:40 +0600

    add subject

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -2,26 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 import './Activity.css'
+import Subject from '../Subject/Subject';
 
 const Activity = () => {
-    const [sub, setSub] = useState([]);
+    const [subjects, setSubjects] = useState([]);
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
-            .then(data => setSub(data))
+            .then(data => setSubjects(data))
 
     }, []);
     return (
 
         <div className='activity-container'>
-            <div>
-                <FontAwesomeIcon icon={faBook} />
+            <div className='header'>
+                <FontAwesomeIcon className='icon' icon={faBook} />
                 <h2>Study-Active-Club</h2>
             </div>
             <h4>Select today's Subject to Study</h4>
             <div className='subjects-container'>
                 {
-                    sub.map(subject => console.log(subject.title))
+                    subjects.map(subject => <Subject
+                        key={subject.id}
+                        subject={subject}
+                    ></Subject>)
                 }
 
             </div>
--- src/components/Subject/Subject.js
+++ src/components/Subject/Subject.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './Subject.css';
 
-const Subject = () => {
+const Subject = (props) => {
+    const { title, img, time, Class } = props.subject;
     return (
-        <div>
+        <div className='subject'>
+            <img src={img} alt="" />
+            <div className='subject-info'>
+                <p >{title}</p>
+                <p>Class: {Class}</p>
+                <p><small>Time:  {time} min</small></p>
+
+            </div>
+            <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
+                <p className='btn-text'>Add to list</p>
+
+            </button>
 
         </div>
     );
